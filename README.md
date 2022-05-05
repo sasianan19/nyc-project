5161e5025cb25e814bfb033756f533d51b409045
sasianan19 <sasianan19@gmail.com>
2022-05-05 15:01:32 -0600

    new nyc project

---
+++ README.md
@@ -0,0 +1,43 @@
+### NYC Guide Project- Django
+**Sierra Nash** *sasianan19*
+**Jon Then** *jonfaustothen*
+**Patrice Smith** *patrice17-20*
+
+# New York City Guide Project-Django #
+
+## **How To *Create* a Virtual Environment** ##
+
+1. Make sure python or it's latest version is installed in your editor. You can do this by running 
+*python --version*
+
+2. After making sure python is properly installed, you can *call* your virtual environment with
+ *python3 -m venv venv*. 
+ **The second *venv* in this command is the NAME of your environment**
+
+## 2. How to *Run* Your Virtual Environment
+#### Window Users:
+1. venv/Scripts/Activate.bat
+
+#### Bash Users:
+1. source venv/ scripts/activate
+
+#### MacOS users:
+1. source/venv/bin/activate
+
+## 3. Installing Dependencies
+
+1. pip install -R Requirements.txt
+
+      **or**
+
+2. pip3 install pipenv
+
+## 4. Running Django
+1. python manage.py runserver
+
+       **or**
+
+ 2. pipenv install django
+
+
+
---
+++ nyc/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.views import View
+
+from nyc.boroughs import boroughs
+
+
+class CityView(View):
+    def get(self, request):
+        return render(request=request, template_name='city.html', context={'boroughs': boroughs.keys()})
+
+
+class BoroughView(View):
+    def get(self, request, borough):
+        return render(
+            request=request,
+            template_name='borough.html',
+            context={'borough': borough, 'activities': boroughs[borough].keys()},
+        )
+
+
+class ActivityView(View):
+    def get(self, request, borough, activity):
+        return render(
+            request=request,
+            template_name='activity.html',
+            context={'borough': borough, 'activity': activity, 'venues': boroughs[borough][activity].keys()}
+        )
+    
+
+
+class VenueView(View):
+    def get(self, request, borough, activity, venue):
+        return render(
+            request=request,
+            template_name="venue.html",
+            context={'borough': borough, 'activity': activity, 'venue': venue, 'description': boroughs[borough][activity][venue]['description']}
+        ) 
+    
